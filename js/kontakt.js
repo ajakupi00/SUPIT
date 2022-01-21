1a7570d248cf9bbf47762ed11a907316a9f9aa02
arjan997 <aapps333@gmail.com>
2022-01-22 00:25:09 +0100

    Pri kraju

---
+++ js/kontakt.js
@@ -0,0 +1,16 @@
+
+
+var modalbox = document.getElementsByClassName("modal")[0];
+var modal_setting = modalbox.style.display;
+var forma = document.getElementsByTagName("form")[0];
+
+function pokaziModal() {
+    modal_setting = "block";
+    modalbox.style.display = modal_setting;
+}
+
+function Odustani() {
+    modal_setting = "";
+    modalbox.style.display = modal_setting;
+}
+
---
+++ js/novost.js
@@ -0,0 +1,20 @@
+var title = document.title;
+var l = title.length;
+var n = Number(title[l - 1]);
+
+$(document).ready(function() {
+    switch (n) {
+        case 1:
+            $("#naslov").css("background-image", "url('/Materials/img/novost1.jpg')");
+            break;
+        case 2:
+            $("#naslov").css("background-image", "url('/Materials/img/novost2.jpg')");
+            break;
+        case 3:
+            $("#naslov").css("background-image", "url('/Materials/img/novost3.jpg')");
+            break;
+        default:
+            break;
+    }
+  
+  });
